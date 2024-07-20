b3cdc660253323ab56d7bcf581cadac010d8a884
fede <cualkiera68@gmail.com>
2024-07-20 19:18:13 -0300

    cleared data

--- src/gcp_credentials.py
+++ src/gcp_credentials.py
@@ -1,8 +1,8 @@
 # Reemplazar service_account_path con la ruta de su archivo .json de autenticacion BigQuery key
-service_account_path = 'credentials.json'
+service_account_path = 'tus_credenciales.json'
 # Reemplazar project_id con el ID del proyecto en BigQuery (se asume que ya fue creado)
-project_id = 'sonic-silo-429813-s7'
+project_id = 'tu_project_id'
 # Reemplazar dataset_id con el ID del dataset en BigQuery (se asume que ya fue creado)
-dataset_id = 'tweets_fgantov'
+dataset_id = 'tu_dataset_id'
 # Reemplazar table_id con el ID de la tabla en BigQuery
-table_id = 'tweets_banana'
+table_id = 'tweets'
